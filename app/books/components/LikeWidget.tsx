09f26eb83d8347ef9271fc61e5a35750d94ff731
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-11-20 14:51:52 +0100

    metadata

--- app/books/components/LikeWidget.tsx
+++ app/books/components/LikeWidget.tsx
@@ -1,42 +1,45 @@
-"use client";
+'use client';
 
-
-import {useOptimistic, useState, useTransition} from "react";
-import {increaseLike} from "@/app/books/components/like.action";
-import Header from "@/app/books/components/Header";
+import { useOptimistic, useState, useTransition } from 'react';
+import { increaseLike } from '@/app/books/components/like.action';
+import Header from '@/app/books/components/Header';
 
 type LikeWidgetProps = {
-	id: string;
-	likes: number;
-}
+  id: string;
+  likes: number;
+};
 
-export default function LikeWidget({id, likes}: LikeWidgetProps) {
+export default function LikeWidget({ id, likes }: LikeWidgetProps) {
+  const [result, setResult] = useState('');
 
-	const [result, setResult] = useState("")
+  const [isPending, startTransition] = useTransition();
+  const [optimisticLike, setOptimisticLike] = useOptimistic(
+    likes,
+    (currentLikes, amount: number) => {
+      return currentLikes + amount;
+    }
+  );
 
-	const [isPending, startTransition] = useTransition();
-	const [optimisticLike, setOptimisticLike] = useOptimistic(likes, (currentLikes, amount: number) => {
-		return currentLikes + amount;
-	});
+  // CLIENT!
+  function handleLike() {
+    startTransition(async () => {
+      setOptimisticLike(1);
+      //                      v------------------- SERVER-CALL!
+      const response = await increaseLike(id);
+      setResult(response);
 
+      // onUpdate(updatedBook);
+    });
+  }
 
-	// CLIENT!
-	function handleLike() {
-		startTransition(async () => {
-			setOptimisticLike(1);
-			//                      v------------------- SERVER-CALL!
-			const response = await increaseLike(id);
-			setResult(response);
-
-			// onUpdate(updatedBook);
-		})
-	}
-
-	return <span >
-		<Header />
-		{likes}
-		<span onClick={() => handleLike()} className={"cursor-pointer underline"}>Like!</span>
-		{result}
-	</span>
-
+  return (
+    <span>
+      <Header />
+      {likes}
+      <span onClick={() => handleLike()} className={'cursor-pointer underline'}>
+        Like!
+      </span>
+      {result}
+    </span>
+  );
 }
--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,71 +1,84 @@
 import ky from 'ky';
-import {NextPage} from 'next';
-import {Book} from '../types/book';
-import {ReactNode} from 'react';
+import { Metadata, NextPage } from 'next';
+import { Book } from '../types/book';
+import { ReactNode } from 'react';
 import TableRow from './components/TableRow';
-import Header from "@/app/books/components/Header";
+import Header from '@/app/books/components/Header';
+
+// export const metadata: Metadata = {
+//   title: 'Books List',
+//   description: 'List of books',
+// };
+
+export async function generateMetadata() {
+  const books = await ky<Book[]>('http://localhost:3001/books').json();
+  return {
+    title: `Dynamic Books List 🚨 (${books.length})`,
+    description: 'List of books',
+  };
+}
 
 const BooksListPage: NextPage = async () => {
-	console.log("BookListPage");
+  console.log('BookListPage');
 
-	let errorMessage: ReactNode = '';
-	let books: Book[] = [];
+  let errorMessage: ReactNode = '';
+  let books: Book[] = [];
 
-	// Führt zu dynamischem Render:
-	//   const meineCookies = cookies();
+  // Führt zu dynamischem Render:
+  //   const meineCookies = cookies();
 
-	// const API_KEY = process.env.MY_SECRET_API_KEY;
+  // const API_KEY = process.env.MY_SECRET_API_KEY;
 
-	try {
-		books = await ky<Book[]>('http://localhost:3001/books').json();
-	} catch (error) {
-		const errorText =
-			error instanceof Error ? error.message : 'An error occurred';
-		errorMessage = (
-			<p className="text-red-500 bg-red-100 border border-red-400 rounded p-2">
-				{errorText}
-			</p>
-		);
-	}
+  try {
+    books = await ky<Book[]>('http://localhost:3001/books').json();
+  } catch (error) {
+    const errorText =
+      error instanceof Error ? error.message : 'An error occurred';
+    errorMessage = (
+      <p className="text-red-500 bg-red-100 border border-red-400 rounded p-2">
+        {errorText}
+      </p>
+    );
+  }
 
-	if (books.length === 0) {
-		return (
-			<div>
-				{errorMessage && <p>{errorMessage}</p>}
-				<p>No books found</p>
-			</div>
-		);
-	}
+  if (books.length === 0) {
+    return (
+      <div>
+        {errorMessage && <p>{errorMessage}</p>}
+        <p>No books found</p>
+      </div>
+    );
+  }
 
-	return (
-		<>
-			<Header />
-			{errorMessage && <p>{errorMessage}</p>}
-			<table className="min-w-full divide-y divide-gray-200">
-				<thead className="bg-gray-50">
-					<tr>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-							ISBN
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-							Titel
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-							Autor
-						</th>
-						<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-							Likes
-						</th>
-					</tr>
-				</thead>
-				<tbody className="bg-white divide-y divide-gray-200">
-					{books.map((book) => (
-						<TableRow book={book} key={book.id}/>
-					))}
-				</tbody>
-			</table>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      {errorMessage && <p>{errorMessage}</p>}
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              ISBN
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Titel
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Autor
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Likes
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {books.map((book) => (
+            <TableRow book={book} key={book.id} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default BooksListPage;
