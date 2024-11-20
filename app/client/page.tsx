cef5463e219717182c9caf35a015f922fbd06f6e
Nils Hartmann <nils@nilshartmann.net>
2024-11-20 11:25:03 +0100

    transition + optimistic

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import {NextPage} from 'next';
-import {use, useContext, useEffect, useState} from 'react';
+import {use, useContext, useEffect, useOptimistic, useState, useTransition} from 'react';
 import {Book} from '@/app/types/book';
 import ky from 'ky';
 import CurrencyProvider, {CurrencyContext} from "@/app/client/CurrencyContext";
+import {ErrorBoundary} from "react-error-boundary";
 
 type ButtonProps = {
 	onClick: () => void;
@@ -25,9 +26,10 @@ const Button: React.FC<ButtonProps> = ({onClick, children}) => {
 type TableRowProps = {
 	book: Book;
 	onDelete: (id: string) => void;
+	onUpdate: (updatedBook: Book) => void;
 };
 
-const TableRow: React.FC<TableRowProps> = ({book, onDelete}) => {
+const TableRow: React.FC<TableRowProps> = ({book, onDelete, onUpdate}) => {
 	console.log('TableRow');
 	return (
 		<tr className="bg-white hover:bg-gray-100">
@@ -41,7 +43,7 @@ const TableRow: React.FC<TableRowProps> = ({book, onDelete}) => {
 				{book.author}
 			</td>
 			<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-				<LikeWidget likes={book.likes}/>
+				<LikeWidget likes={book.likes} bookId={book.id} onUpdate={onUpdate}/>
 			</td>
 			<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
 				<PriceWidget title={book.title} price={book.price}/>
@@ -55,11 +57,41 @@ const TableRow: React.FC<TableRowProps> = ({book, onDelete}) => {
 
 type LikeWidgetProps = {
 	likes: number;
+	bookId: string;
+	onUpdate(updatedBook: Book): void
 }
 
-const LikeWidget: React.FC<LikeWidgetProps> = ({likes}) => {
+const LikeWidget: React.FC<LikeWidgetProps> = (props) => {
+	return <ErrorBoundary fallback={<div>Fehler aufgetreten!</div>}>
+		<LikeWidgetInternal {...props} />
+	</ErrorBoundary>
+}
+
+const LikeWidgetInternal: React.FC<LikeWidgetProps> = ({likes, bookId, onUpdate}) => {
 	// todo: implement with increasing Likes with useTransition and useOptimistic
-	return <span>{likes}</span>
+
+	const [isPending, startTransition] = useTransition();
+	const [optimisticLike, setOptimisticLike] = useOptimistic(likes, (currentLikes, amount: number) => {
+		return currentLikes + amount;
+	});
+
+	const handleLikeClick = () => {
+		startTransition(async () => {
+			setOptimisticLike(1);
+			const updatedBook = await ky.patch<Book>(`http://localhost:3001/books/${bookId}/likes?slowdown=2400`).json();
+			// updatedBook.likes = 666;
+			onUpdate(updatedBook);
+		})
+	}
+
+	return <div className={"flex"}>
+		{optimisticLike} ({likes})
+		<button className={"cursor-pointer hover:underline"} onClick={() => handleLikeClick()} disabled={isPending}>
+			Like me!
+		</button>
+		{isPending && "Liking..."}
+
+	</div>
 
 }
 
@@ -82,7 +114,6 @@ const PriceWidget: React.FC<{ title: string; price: number }> = ({title, price})
 
 	// const currencyContext = useContext(CurrencyContext);
 	const currencyContext = showPrices ? use(CurrencyContext) : null;
-
 
 	// const router = useRouter();
 
@@ -112,6 +143,10 @@ const PriceWidget: React.FC<{ title: string; price: number }> = ({title, price})
 const ClientPage: NextPage = () => {
 	const [books, setBooks] = useState<Book[]>([]);
 
+	const [isPending, startTransition] = useTransition();
+
+	// ky-Bibliothek: https://github.com/sindresorhus/ky
+
 	useEffect(() => {
 		ky<Book[]>('http://localhost:3001/books')
 			.json()
@@ -127,12 +162,19 @@ const ClientPage: NextPage = () => {
 		setBooks(books => books.map(b => b.id === book.id ? book : b));
 	}
 
+
+
 	return (
 		<table className="min-w-full divide-y divide-gray-200">
 			<thead className="bg-gray-50">
 				<tr>
 					<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-						ISBN
+						ISBN {String(isPending)}
+						<button onClick={() => {
+							startTransition(async () => {
+								return new Promise( res =>setTimeout(res, 5000));
+							})
+						}}>Start</button>
 					</th>
 					<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 						Titel
@@ -151,7 +193,7 @@ const ClientPage: NextPage = () => {
 			</thead>
 			<tbody className="bg-white divide-y divide-gray-200">
 				{books.map((book) => (
-					<TableRow book={book} key={book.id} onDelete={handleDelete}/>
+					<TableRow book={book} key={book.id} onDelete={handleDelete} onUpdate={handleUpdate}/>
 				))}
 			</tbody>
 		</table>
@@ -159,7 +201,7 @@ const ClientPage: NextPage = () => {
 };
 
 function ClientPageApp() {
-	return <CurrencyProvider><ClientPage /></CurrencyProvider>
+	return <CurrencyProvider><ClientPage/></CurrencyProvider>
 }
 
 export default ClientPageApp;
